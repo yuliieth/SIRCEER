1c251c8e66cdfbdaf9979091b6dee0c30df52b2b
Alexis <titiruizah@gmail.com>
2017-04-17 21:40:53 -0500

    Se crea la estructura semantica del index asministrativo

--- view/index_admin.view.php
+++ view/index_admin.view.php
@@ -1,10 +1,31 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Index admin</title>
+	<title>Sistema de registro y control estudiantes de risaralda</title>
+	<link rel="stylesheet" href="../	css/estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
 </head>
 <body>
-	
+
+	<header>
+		<h1>modulo de administracion</h1>
+		
+		<div class="nav-bar">
+			<ul>
+				<li><a href="view/login.php">Empecemos</a></li>
+			</ul>
+		</div>
+	</header>
+
+
+	<div class="contenedor">
+		
+	</div>
 </body>
+<footer>
+	<p>Todos los derechos reservados</p>
+	<p>2017 Cristhian Alexis Galeano Ruiz </p>
+</footer>
 </html>
