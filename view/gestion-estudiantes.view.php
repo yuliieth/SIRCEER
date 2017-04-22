fc1bd192a728335eb8d677d9b4e30f1244584b14
Alexis <titiruizah@gmail.com>
2017-04-21 22:09:31 -0500

    Agrego un table para empezar a diseñar el submenu en este apartado

--- view/gestion-estudiantes.view.php
+++ view/gestion-estudiantes.view.php
@@ -1,3 +1,11 @@
-<?php require'cabecera-admin.php' ?>
-gestion estudiantes
+<?php require 'cabecera-admin.php' ?>
+	
+<table>
+	<tr>
+		<td>Area de trabajo</td>
+		<br/>
+		<td>submenu</td>
+	</tr>
+</table>
+
 <?php require'piedepagina-admin.php' ?>
