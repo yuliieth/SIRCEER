1b75ed08556e45555702984cbb4fedfa3df85896
Alexis <titiruizah@gmail.com>
2017-04-25 20:59:00 -0500

    Agregue la validacion en php del envio por post del frmlario de new student

--- admin/new_estudiante.php
+++ admin/new_estudiante.php
@@ -1,2 +1,24 @@
+<?php 
+	$errores = "";
+
+	if (isset($_POST['submit'])) {
+		$documento = $_POST['documneto'];
+		$primerNombre = $_POST['primer-nombre'];
+		$segundoNombre = $_POST['segundo-nombre'];
+		$primerApellido = $_POST['primer-apellido'];
+		$segundoApellido = $_POST['segundo-apellido'];
+		$direccion = $_POST['direccion'];
+		$municipio = $_POST['municipio'];
+		$celular = $_POST['celular'];
+		$telefono = $_POST['telefono'];
+		$email = $_POST['email'];
+		$fechaNaci = $_POST['fecha-naci'];
+		$lugarNaci = $_POST['lugar-naci'];
+		$estrato = $_POST['estrato'];
+		$desplazado = $_POST['desplazado'];
+		$afrodescendiente = $_POST['afrodescendiente'];
+		$ojos = $_POST['ojos'];
+	
+	}
+ ?>
 <?php require("../view/new_estudiantes-view.php") ?>
-http://pventa.softwareezquivel.com/
