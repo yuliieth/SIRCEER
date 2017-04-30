33bd66545a409a374c97e654e238ba04eaac0f37
Alexis <titiruizah@gmail.com>
2017-04-30 13:04:36 -0500

    Se trabajo sobre el frm de login, se trabajo estilos: terminado

--- view/loguin.view.php
+++ view/loguin.view.php
@@ -4,23 +4,24 @@
 	<meta charset="UTF-8">
 	<title>Login</title>
 	<link rel="stylesheet" href="../css/estilos.loguin.css">
+	<link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
 </head>
 <body>
 	<div class="contenedor">
 		<div class="contenedor-loguin">
-			<div class="barra-loguin">
-			</div>
 				<div class="contenedor-form">
-					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="frm-login">
+					<i class="fa fa-user-circle-o" aria-hidden="true"></i>
 						<input type="text" name="usuario" placeholder="Nombre de usuario">
 						<br>
-						<input type="text" name="pass" placeholder="Contraseña">
+						<i class="fa fa-key" aria-hidden="true"></i>
+						<input type="password" name="pass" placeholder="Contraseña">
 						<br>
-						<input type="submit" value="Entrar">
+						<input type="submit" class="btn" value="Entrar">
 					</form>
 			</div>
-		</div>
+		
 	</div>
 </body>
 </html>
