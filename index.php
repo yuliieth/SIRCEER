b3f527b3df8217af191f65dfa0149e44cc7c5e18
Alexis <titiruizah@gmail.com>
2017-04-30 13:56:52 -0500

    Actualice estilos del index principal

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sistema de registro y control estudiantes de risaralda</title>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/estilos.css">
 	<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
 </head>
@@ -24,7 +25,13 @@
 
 	<div class="contenedor">
 		<p id="wellcome">Te damos la bienvenida a tu nuevo gestor de estudiantes </p>
-		<p id="features">Podras <strong>REGISTRAR</strong>, <strong>BUSCAR</strong> y <strong>ACTUALIZAR</strong></p>
+		<p id="features">Podras <br> <strong>REGISTRAR</strong>, <strong>BUSCAR</strong> y <strong>ACTUALIZAR</strong></p>
+		<div class="imagenes-bienvenida">
+			<i class="fa fa-plus fa-5x" id="img-index" aria-hidden="true"></i>
+			<i class="fa fa-search-plus fa-5x" id="img-index"  aria-hidden="true"></i>
+			<i class="fa fa-pencil-square-o fa-5x" id="img-index" aria-hidden="true"></i>
+
+		</div>
 	</div>
 </body>
 <footer>
