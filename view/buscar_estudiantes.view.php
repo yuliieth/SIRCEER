b9f105ce821dfbeaaf84ad7d4972ce6629bf4e82
garyRuiz <titiruizah@gmail.com>
2017-05-17 17:32:03 -0500

    Se trabajo en la busquedad de estudiantes se agregararon tres archivos los cuales por ahora van a estar ubicados en la misma carpeta

--- view/buscar_estudiantes.view.php
+++ view/buscar_estudiantes.view.php
@@ -1,18 +1,10 @@
 <?php require("cabecera-admin.php") ?>
 
-<div class="formulario">
-	<form action="" method="POST">
-		<input type="text" name="documento" placeholder="Documento de identidad">
-		<input type="text" name="nombres" placeholder="Nombres">
-		<input type="text" name="apellidos" placeholder="Apellidos">
-		<input type="text" name="direccion" placeholder="Direccion">
-		<input type="text" name="celular" placeholder="Celular">
-		<input type="text" name="telefono" placeholder="Telefono">
-		<input type="text" name="email" placeholder="E-mail">
-		<input type="text" name="fecha-naci" placeholder="fecha de nacimiento">
-		<input type="text" name="lugar-naci" placeholder="Lugar de nacimiento">
-		<input type="submit" value="Enviar">
-	</form>
-</div>
+<section>
+	<input type="text" name="busqueda" id="busqueda" placeholder="Buscar">
+</section>
+<section id="tabla_resultado">
+	<!--Aqui se desplegara nuestra tabla consulta-->
+</section>
 
 <?php require("piedepagina-admin.php") ?>
--- view/cabecera-admin.php
+++ view/cabecera-admin.php
@@ -5,7 +5,8 @@
 	<title>Sistema de registro y control estudiantes de risaralda</title>
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="peticion_busquedad.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
 	<link rel="stylesheet" href="../css/estilos.css">
 </head>
