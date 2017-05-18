d41a42d36d4087c2e7d905f581d910a5815b50a6
garyRuiz <titiruizah@gmail.com>
2017-05-18 17:55:02 -0500

    Se esta reestructurando la pagina

--- index.php
+++ index.php
@@ -4,48 +4,92 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sistema de registro y control estudiantes de risaralda</title>
-	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/estilos.css">
-	<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
+	<link rel="stylesheet" href="iconos/css/fontello.css">
+	<link rel="stylesheet" href="css/estilos-index.css">
+	<meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximun-scale=1.0,miimun=1.0">
 </head>
 <body>
 
 	<header>
-		<h1>Sistema para el registro y control de estudiantes</h1>
-		
-		<div class="nav-bar">
+		<img src="imagenes/logo_magtimus3.png" class="img-logo" height="80" width="100" alt="">
+		<input type="checkbox" id="check">
+		<label for="check" class="icon-menu"></label>
+
+		<div class="menu">
 			<ul>
-				<li><a href="admin/loguin.php">Empecemos</a></li>
+				<li><a href="#" style="background:#df6666;color:white;">Inicio</a></li>
+				<li><a href="#">Acerca de</a></li>
+				<li><a href="#">Servicios</a></li>
+				<li><a href="#">Ingresar al SRCER</a></li>
 			</ul>
 		</div>
 
-
 	</header>
 
-
-	<div class="contenedor">
-		<p id="wellcome">Te damos la bienvenida a tu nuevo gestor de estudiantes </p>
-		<p id="features">Podras <br> <strong>REGISTRAR</strong>, <strong>BUSCAR</strong> y <strong>ACTUALIZAR</strong></p>
-		<div class="imagenes-bienvenida">
-			<i class="fa fa-plus fa-5x" id="img-index" aria-hidden="true"></i>
-			<i class="fa fa-search-plus fa-5x" id="img-index"  aria-hidden="true"></i>
-			<i class="fa fa-pencil-square-o fa-5x" id="img-index" aria-hidden="true"></i>
-
+	<!--Aqui empieza el cuerpo de la pagina-->
+	<main>
+		<div class="content-one">
+			<div class="content-portada">
+				<div class="content-item1">
+					<h4>It Doesn't Take a Rocket <h3>Scientist</h3> </h4>
+                    <h2>IT TAKES A DESIGNER</h2>
+				</div>
+			</div>
 		</div>
-	</div>
-</body>
+
+		<div class="content-two">
+			<div class="content-details">
+				<div class="content-items2">
+					<label class="icon-heart"></label>
+					<h4>Design</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quisquam numquam rem! Ad rerum, asperiores architecto repellat vero odit omnis itaque voluptatem excepturi maxime animi, quo nam laudantium doloremque et.</p>
+				</div>
+				<div class="content-items2">
+					<label class="icon-lapto"></label>
+					<h4>Bootstrap</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident enim et beatae deserunt quo aspernatur laudantium ratione necessitatibus nulla aut, magnam maxime quisquam dolores temporibus nobis, molestiae, doloremque quod minus.</p>
+				</div>
+				<div class="content-items2">
+					<label class="icon-support"></label>
+					<h4>Support</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis porro iste nemo labore natus quibusdam beatae tenetur, eum numquam. Numquam expedita pariatur fuga, quia explicabo fugiat aut. Nemo, in, odio?</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="content-three">
+			<div class="content module">
+				<h4>LATEX WORK</h4>
+				<img src="imagenes/img2.png" alt="">
+				<img src="imagenes/img3.png" alt="">
+				<img src="imagenes/img4.png" alt="">
+			</div>
+		</div>
+
+		<div class="content-four">
+			<div class="content-item3">
+				<h4>Creamos nuestra primera class de diseño</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam consectetur incidunt vel quas? Magnam, quae molestias nesciunt sunt officia beatae perspiciatis ipsam nostrum nihil, vel nisi, ad? Blanditiis aspernatur, iure!</p>
+				<img src="imagenes/img5.png" alt="">
+			</div>
+		</div>
+
+		<div class="content-five">
+			<div class="content-item4">
+				<h4>WE ARE STORYTELLERS. BRANDS ARE OUR SUBJECTS. DESIGN IS OUR VOICE.</h4>
+                    <p>We believe ideas come from everyone, everywhere. At BlackTie, everyone within our agency walls is a designer in their own right. And there are a few principles we believe—and we believe everyone should believe—about our design craft. These truths drive us, motivate us, and ultimately help us redefine the power of design.</p>
+			</div>
+		</div>
+
+	</main>
+
+
+	
 <footer>
-
-	
-	<p>Todos los derechos reservados</p>
-	<p>Gobernacion de risaralda °2017</p>
-
-	<ul>
-		<li><img src="imagenes/gobernacion.PNG" alt="gobernacion" height="100" width="100"><a href="#"></a></li>
-		<li><img src="imagenes/pereira-escudo.PNG" alt="Pereira" height="100" width="100" alt=""><a href="#"></a></li>
-		<li><img src="imagenes/colombia.jpg" alt="Colombia" height="100" width="100" alt=""><a href="#"></a></li>
-	</ul>
-	
-
+	<footer>
+        <label class="icon-twitter"></label>
+        <label class="icon-facebook"></label>
+        <label class="icon-instagram"></label>
+    </footer>
 </footer>
 </html>
