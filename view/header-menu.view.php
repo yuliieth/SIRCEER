d01eaf7b158cab85b21e0521524688395a3c9d2f
garyRuiz <titiruizah@gmail.com>
2017-06-29 11:35:59 -0500

    Se crearon dos archivos .php para la cabecera del menu y otro para el footer del menu estos dos archivos su finalidad es llamarlos en cada pagina y que sus links sean dinamicos, a continuacion se procede a crear la logica php para ello

---
+++ view/header-menu.view.php
@@ -0,0 +1,23 @@
+<table id="estructura">
+	<tr>
+		<td id="menu">&nbsp;
+			<ul>
+				<li>
+                <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
+                <a href="../gestion/new_estudiante.php">Nuevo</a>
+                </li>
+				<li>
+                <i class="fa fa-search-plus fa-2x" aria-hidden="true"></i>
+                <a href="../gestion/buscar_estudiantes.php">Buscar</a>
+                </li>
+				<li>
+                <i class="fa fa-flag-checkered fa-2x" aria-hidden="true"></i>
+                <a href="../gestion/reportes-estudiantes.php">Reportes</a>
+                </li>
+				<li>
+                <i class="fa fa-pie-chart fa-2x" aria-hidden="true"></i>
+                <a href="../gestion/estadisticas-estudiantes.php">Estadisticas</a>
+                </li>
+			</ul>
+		</td>
+		<td id="pagina">
