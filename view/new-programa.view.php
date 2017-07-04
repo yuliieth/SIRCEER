93f506a2bf0a5aad72e6f70eeb47c69700569bd0
garyRuiz <titiruizah@gmail.com>
2017-07-04 10:51:52 -0500

    Se creo la vista con los campos respectivos hasta el momento con su archivo php sin logica total de programacion

--- view/new-programa.view.php
+++ view/new-programa.view.php
@@ -6,30 +6,13 @@
 
 	<!--<table class="table-formulario">-->
 	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-		
-		
 		<input type="text" size="30" name="nombre" placeholder="Nombre*" required>
-		<input type="text" size="30" name="codigo" placeholder="Codigo*" required="">
-		<input type="text" size="30" name="telefono" placeholder="Telefono">		
+		<input type="text" size="30" name="codigosnies" placeholder="Codigo SNIES*" required>
+		<input type="text" size="30" name="semestres" placeholder="Semestres*">		
 		<br>
-		<select  name="municipio" id="municipio">
-			<optgroup label="Risaralda">
-				<option value="pereira">Pereira</option>
-				<option value="dosquebradas">D/Quebradas</option>
-				<option value="santa rosa">Santa rosa</option>
-				<option value="apia">Apia</option>
-				<option value="mistrato">Mistrato</option>
-				<option value="belen de umbria">Belen de umbria</option>
-				<option value="chinchina">Chinchina</option>
-			</optgroup>
-		</select>		
-		<input type="email" size="30" name="email" placeholder="E-mail*" required="">	
-		<input type="text" size="30" name="direccion" placeholder="Direccion">
-		
-		
-		<br>
-		
-		
+		<input type="text" size="30" name="creditos" placeholder="Creditos*" required>	
+		<input type="text" size="30" name="nivel-academico" placeholder="Nivel academico">
+		<br>		
 		<?php if (!empty($errores)): ?>
 			<div class="input-redit alert error">
 				<?php echo $errores;?>
@@ -39,19 +22,11 @@
 				<p>Datos enviados correctamente</p>
 			</div>
 		<?php endif ?>
-		
-
-		
 		<input type="reset" name="">
 		<input type="submit" name="submit" class="btn btn-primary" value="Guardar">
 
 	</form>
 	<!--</table>-->
 </div>
-
 <!--END CONTENIDO-->				
-
-
-
-
 <!--<?php require("piedepagina-admin.php") ?>-->
