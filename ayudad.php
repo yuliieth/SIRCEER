6ee9ee8e06e5efda815aa485bf45ef4f1fddb9f9
garyRuiz <titiruizah@gmail.com>
2017-07-11 16:11:51 -0500

    Se agregaronnuevos cursos

---
+++ ayudad.php
@@ -0,0 +1,30 @@
+<body>
+
+	<CENTER><font size=10 face="Cambria,verdana">Thriller (1982)</font></CENTER>
+
+</br>
+
+<table align="center" border="5" width="400" cellpadding="20" cellspacing="1">
+
+	<tr>
+
+		<td> 
+			<input name="boton3" type=image src="https://images-na.ssl-images-amazon.com/images/I/71fV4tt8nmL._SL1500_.jpg" width=500 height=500 /> 
+			<CENTER> 
+			<form method="get" action="final.php">
+					<input type="hidden" name="gasto" value="<?php echo 850 ;?>"> 
+					<input type="submit" value="Comprar álbum" /> 
+				</form> 
+				<CENTER> 
+		</td>
+
+	</tr>
+
+
+
+			</body>
+
+			</html>
+
+
+
---
+++ final.php
@@ -0,0 +1,19 @@
+ <?php
+ #var_dum() permite conocer el tipo y el contenido de una variable
+  #var_dump($_GET); 
+
+#Ahora que ya sabemos que hemos podido atrapar ese valor desde el otro archivo ahora si podemos hacer lo que queramos		
+    $preciorock = 900;
+
+    $preciopop = 850;
+
+    $preciornb = 700;
+
+# Y mostramos el valor
+    echo "El valor del disco es:".$_GET['gasto'];
+
+    ?>
+
+    </body>
+
+    </html>
