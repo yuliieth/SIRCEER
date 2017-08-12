bdeba37d4a08007dd6e3910d094eb776a36fc7f7
Alexis <titiruizah@gmail.com>
2017-08-11 21:34:03 -0500

    Se crearon los archivos vista y archivo que contendra la logica de la gestion del estudiante

--- gestion/gestionar-estudiante.php
+++ gestion/gestionar-estudiante.php
@@ -1,2 +1,14 @@
-
+<?php session_start(); ?>
+<?php  require_once '../php/funciones.php';
+validateSession();
+?>
+<?php 
+#Necesidades:
+/*
+1. El estudiante a mostrar (Ya esta seleccionado).
+2. Frm para diligencias la tabla semestre.
+3. Traer todos los programas registrados para ser seleccionados (Esta tabla ya estara diligenciada, ademas es una tabla hija por lo cual debemos tener cuidado de diligneciar sus tablas padres ).
+4. Finalmente con estos datos hacer un insert para "evaluacion_semestral"
+*/
+?>
 <?php require '../view/gestionar-estudiante.view.php' ?>
---
+++ view/gestionar-estudiante.view.php
@@ -0,0 +1,4 @@
+<?php require 'cabecera-admin.php';?>
+<?php require("header-menu.view.php") ?>
+<?php require("footer-menu.view.php") ?>
+<?php require'piedepagina-admin.php' ?>
