ca0aa94bc270c38da09bb2ad549233d55c33c99f
garyRuiz <titiruizah@gmail.com>
2017-10-18 17:58:23 -0500

    Se trabajo en la opcion ver estudiante, se adelante en parte un diseño mas limpio

--- view/ver-estudiante.view.php
+++ view/ver-estudiante.view.php
@@ -1,66 +1,41 @@
 <?php 
 $html = '
-<header>
-      <div id="logo">
-        <img src="../imagenes/gobernacion1.jpg">
+<div style="margin-top:10px; width:100%;">
+      <div style=" width:100%; background: white; margin-top: 12px; text-align:center; font-size: .1em;">
+          <h2>GOBERNACION DE RISARALDA</h2>
+      <div style="width:90;float:right;">
+          <img style="80%;" src="../imagenes/gobernacion1.jpg">
       </div>
-      <h1><strong>GOBERNACION DE RISARALDA</strong></h1>
-      <div style = "background: #fff;">
-        <div >Secretaria de eduacion de Risaralda</div>
-        <p style = "font-family: monospace; color: blue;">Lugar:</p>
-        <div>Dirección: Calle 19 No 13-17-Código Postal 660004 - PEREIRA - RISARALDA - COLOMBIA NIT: 891.480.085-7</div>
-        <div>telefono</div>
-        <div><a href="mailto:company@example.com">company@example.com</a></div>
       </div>
-      
-    </header>
-    <main>
-      <table style = width: 100%;
-  border-collapse: collapse;
-  border-spacing: 0;
-  margin-bottom: 20px;>
 
-  <!--Encabezados-->
-        <thead>
-          <tr>
-            <th style = "font-family: monospace; color: red;">Primer nombre</th>
-            <th style = "font-family: monospace; color: red;">Segundo nombre</th>
-            <th style = "font-family: monospace; color: red;" >Primer apellido</th>
-            <th style = "font-family: monospace; color: red;" >Segundo apellido</th>
-            <th style = "font-family: monospace; color: red;" >Email</th>
-            <th style = "font-family: monospace; color: red;" >Fecha de nacimiento</th>
-            <th style = "font-family: monospace; color: red;" >Edad</th>
-            <th style = "font-family: monospace; color: red;" >Estrato</th>
-            <th style = "font-family: monospace; color: red;" >Zona</th>
-            <th style = "font-family: monospace; color: red;" >Desplazado</th>
-          </tr>
-        </thead>
-        <tbody>';
 
-        $html .= ' 
-          <tr>
-            <td style = text-align: center;>'.$estudiante['primer_nombre'].'</td>
-            <td style = text-align: center;>'.$estudiante['segundo_nombre'].'</td>
-            <td class="unit">'.$estudiante['primer_apellido'].'</td>
-            <td class="qty">'.$estudiante['segundo_apellido'].'</td>
-            <td class="qty">'.$estudiante['email'].'</td>
-            <td class="qty">'.$estudiante['fecha_naci'].'</td>
-            <td class="total">'.$estudiante['edad'].'</td>
-            <td class="total">'.$estudiante['estrato'].'</td>
-            <td class="total">'.$estudiante['zona'].'</td>
-            <td class="total">'.$estudiante['desplazado'].'</td>
-          </tr>';
+
+
+      <div style="background-color: white; width: 100%;">
+            <label><strong>Primer nombre</strong></label>&nbsp;&nbsp;
+            <label>'.$estudiante['primer_nombre'].'<label>
+            <label><strong>Segundo nombre</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['segundo_nombre'].'<label><br>
+            <label><strong>Primer apellido</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['primer_apellido'].'<label>
+            <label><strong>Segundo apellido</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['segundo_apellido'].'<label><br>
+            <label><strong>Email</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['email'].'<label>
+            <label><strong>Fecha de nacimiento</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['fecha_naci'].'<label><br>
+            <label><strong>Edad</strong>&nbsp;&nbsp;
+            <label>'.$estudiante['edad'].'<label>
+            <label><strong>Estrato</strong>&nbsp;&nbsp;
+            <label>'.$estudiante['estrato'].'<label>
+            <label><strong>Zona</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['zona'].'<label>
+            <label><strong>Desplazado</strong><label>&nbsp;&nbsp;
+            <label>'.$estudiante['desplazado'].'<label>
+          </div>';
           $html .= '
-        </tbody>
-      </table>
       <div id="notices">
-      <br>
-      <br>
-      <br>
-        <div>NOTICE:</div>
-        <div class="notice">Escribir una leyenda</div>
-      </div>
-    </main>';
+    </div>';
 $mpdf = new mPDF('c','A4');
 #$css = file_get_contents('../css/estilos.css');
 #$mpdf->writeHTML($css);
