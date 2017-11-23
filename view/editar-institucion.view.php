8cf07f8e8eb2cad1bfcc0957f248ab7c9fad358d
garyRuiz <titiruizah@gmail.com>
2017-11-23 09:39:49 -0500

    Componentes de editar institucion han sido acomodados: solucionado

--- view/editar-institucion.view.php
+++ view/editar-institucion.view.php
@@ -4,11 +4,38 @@
 	<h3>Esta modificando una institucion:</h3>
 	<hr>
 	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-		<input type="hidden" size="20" name="id" value="<?php echo $result['id']; ?>">
-		<input type="text" size="20" name="nombre" value="<?php echo $result['nombre']; ?>"  placeholder="Nombre" required="">
-		<input type="text" size="30" name="telefono" value="<?php echo $result['telefono']; ?>"  placeholder="Telefono" required="">
-		<input type="email" size="30" name="email" value="<?php echo $result['email']; ?>"  placeholder="E-mail" required="">
-		<input type="text" size="30" name="direccion" value="<?php echo $result['direccion']; ?>" placeholder="Direccion" ><br>
+
+		<table>
+
+		  <tr>
+		  	<td>Id:</td>
+			<td><input type="text" readonly="readonly" size="20" name="id" value="<?php echo $result['id']; ?>"></td>
+			<td>Nombre:</td>
+			<td><input type="text" size="20" name="nombre" value="<?php echo $result['nombre']; ?>"  placeholder="Nombre" required=""></td>
+		  </tr>
+
+		  <tr>
+		  	<td>Telefono:</td>
+			<td><input type="text" size="30" name="telefono" value="<?php echo $result['telefono']; ?>"  placeholder="Telefono" required=""></td>
+			<td>Email:</td>
+			<td><input type="email" size="30" name="email" value="<?php echo $result['email']; ?>"  placeholder="E-mail" required=""></td>
+		  </tr>
+
+		  <tr>
+		  	<td>Dirección:</td>
+		  	<td><input type="text" size="30" name="direccion" value="<?php echo $result['direccion']; ?>" placeholder="Direccion" ></td>
+		  	<td></td>
+		  	<td></td>
+		  </tr>
+
+		  <tr>
+		  	<td></td>
+		  	<td></td>
+			<td><input type="reset" value="Restaurar"></td>
+			<td><input type="submit" name="submit" class="btn btn-primary" value="Guardar"></td>
+		  </tr>
+
+		</table>
 		
 		
 		<?php if (!empty($errores)): ?>
@@ -23,8 +50,8 @@
 		
 
 		
-		<input type="reset" name="">
-		<input type="submit" name="submit" class="btn btn-primary" value="Guardar">
+		
+		
 
 	</form>
 </div>
