e5071822a903dd8e698dd38f360e84dcf6b5e88e
garyRuiz <titiruizah@gmail.com>
2017-11-23 09:47:13 -0500

    Componentes de editar programa acomodados: solucionado

--- view/editar-programa.view.php
+++ view/editar-programa.view.php
@@ -4,20 +4,46 @@
 	<h3>Esta modificando una institucion:</h3>
 	<hr>
 	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-		<input type="hidden" size="20" name="snies" value="<?php echo $result['snies']; ?>">
-		<input type="text" size="20" name="nombre" value="<?php echo $result['nombre']; ?>"  placeholder="Nombre" required="">
-		<input type="text" size="30" name="num_semestres" value="<?php echo $result['num_semestres']; ?>"  placeholder="Semestres" required="">
-		<input type="text" size="30" name="num_creditos" value="<?php echo $result['num_creditos']; ?>"  placeholder="Creditos" required="">
 
-		
-		
-		<select  name="nivel_academico" id="nivel_academico">
+		<table>
+			<tr>
+				<td>SNIES:</td>
+				<td><input type="text" readonly="readonly" size="20" name="snies" value="<?php echo $result['snies']; ?>"></td>
+				<td>Nombre:</td>
+				<td><input type="text" size="20" name="nombre" value="<?php echo $result['nombre']; ?>"  placeholder="Nombre" required=""></td>
+			</tr>
+
+
+			<tr>
+				<td>Numero de semestres:</td>
+				<td><input type="text" size="30" name="num_semestres" value="<?php echo $result['num_semestres']; ?>"  placeholder="Semestres" required=""></td>
+				<td>Numero de creditos:</td>
+				<td><input type="text" size="30" name="num_creditos" value="<?php echo $result['num_creditos']; ?>"  placeholder="Creditos" required=""></td>
+			</tr>
+
+
+			<tr>
+				<td>Nivel academico:</td>
+				<td><select  name="nivel_academico" id="nivel_academico">
 			<optgroup label="nivel_academico">
 				<option value="1"<?php if ($result['nivel_academico_id'] == 'Tecnico'){echo " selected";}?>>Tecnico</option>
 				<option value="2"<?php if ($result['nivel_academico_id'] == 'Tecnologo'){echo " selected";}?>>Tecnologo</option>
 				<option value="3"<?php if ($result['nivel_academico_id'] == 'Ingenieria'){echo " selected";}?>>Ingenieria</option>
 			</optgroup>
 		</select>		
+		</td>
+				<td></td>
+				<td></td>
+			</tr>
+
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="reset" name=""></td>
+				<td><input type="submit" name="submit" class="btn btn-primary" value="Guardar"></td>
+			</tr>
+		</table>
+		
 		
 		
 		
@@ -33,8 +59,8 @@
 		
 
 		
-		<input type="reset" name="">
-		<input type="submit" name="submit" class="btn btn-primary" value="Guardar">
+		
+		
 
 	</form>
 </div>
