a2ccdd82d375cfd556f6fbc4bac36264d5597757
garyRuiz <titiruizah@gmail.com>
2017-11-23 09:53:45 -0500

    Componentes de editar alianza acomodados: solucionado

--- view/editar-alianza.view.php
+++ view/editar-alianza.view.php
@@ -4,16 +4,38 @@
 	<h3>Esta modificando un estudiante:</h3>
 	<hr>
 	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-		<label for="nombre">ID:</label>
-		<input type="text" size="20" readonly="readonly" value="<?php echo $result['id'] ?>" name="id" required="" >
-		<label for="nombre">Nombre:</label>
-		<input type="text" size="20" value="<?php echo $result['nombre'] ?>" name="nombre" required="" >
-		<label for="fecha_ini">Fecha de inicio:</label>
-		<input type="text" size="30" value="<?php echo $result['fecha_inicio'] ?>" name="fecha_ini" required="required"><br>
-		<label for="fecha_fina">Fecha final:</label>
-		<input type="text" size="20" value="<?php echo $result['fecha_final'] ?>" name="fecha_fina" required="required" >
-		<label for="cupos">Cupos:</label>
-		<input type="text" size="30" value="<?php echo $result['cupos'] ?>" name="cupos"  required="required"><br>
+	 <table>
+		<tr>
+			<td><label for="nombre">ID:</label></td>
+			<td><input type="text" size="20" readonly="readonly" value="<?php echo $result['id'] ?>" name="id" required="" ></td>
+			<td><label for="nombre">Nombre:</label></td>
+			<td><input type="text" size="20" value="<?php echo $result['nombre'] ?>" name="nombre" required="" ></td>
+		</tr>
+
+
+		<tr>
+			<td><label for="fecha_ini">Fecha de inicio:</label></td>
+			<td><input type="text" size="30" value="<?php echo $result['fecha_inicio'] ?>" name="fecha_ini" required="required"></td>
+			<td><label for="fecha_fina">Fecha final:</label></td>
+			<td><input type="text" size="20" value="<?php echo $result['fecha_final'] ?>" name="fecha_fina" required="required" ></td>
+		</tr>
+
+
+		<tr>
+			<td><label for="cupos">Cupos:</label></td>
+			<td><input type="text" size="30" value="<?php echo $result['cupos'] ?>" name="cupos"  required="required"></td>
+			<td></td>
+			<td></td>
+		</tr>
+
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="reset" name=""></td>
+			<td><input type="submit" name="submit" class="btn btn-primary" value="Guardar"></td>
+		</tr>	
+		
+	 </table>
 
 		<?php if (!empty($errores)): ?>
 			<div class="input-redit alert error">
@@ -25,11 +47,6 @@
 			</div>
 		<?php endif ?>
 		
-
-		
-		<input type="reset" name="">
-		<input type="submit" name="submit" class="btn btn-primary" value="Guardar">
-
 	</form>
 </div>
 <?php require 'piedepagina-admin.php' ?>
