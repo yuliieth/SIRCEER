1a3428f757253a0a23265831414f31be693fccdb
garyRuiz <titiruizah@gmail.com>
2017-11-30 14:42:10 -0500

    Se crea animacion con javascript vista terminadad implementar logica en los resultados veridicos

--- view/cabecera-admin.php
+++ view/cabecera-admin.php
@@ -7,38 +7,40 @@
 	<link rel="stylesheet" href="<?php echo URL; ?>css/estilos.css">
 	<link rel="icon" href="<?php echo URL; ?>imagenes/favicon.png">
 	<link rel="stylesheet" href="<?php echo URL; ?>font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="<?php echo URL; ?>css/odometer-theme-car.css" />
 	<!--La sgte libreria es inprescidinble para el buscador-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>-->
+	<script src="<?php echo URL; ?>js/odometer.js"></script>
 	<script src="<?php echo URL; ?>js/jquery.js"></script>
 	<script src="<?php echo URL; ?>js/javascript.js"></script>
 	<script src="../js/jquery.js"></script>
 	<!--<link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">-->
 </head>
 <body>
-<!--https://www.facebook.com/fyupanquia-->
+	<!--https://www.facebook.com/fyupanquia-->
 	<header>
 		<h1><a href="principal-gestion.php">SRCEER</a></h1>
 		<div class="user">
 			<i class="fa fa-user-circle fa-2x" aria-hidden="true"></i><a href="<?php echo URL; ?>php/cerrar-sesion.php"><?php echo $_SESSION['usuario']['user']; ?></a>
 		</div>
-	
+
 	</header>
 
 	<div class="wraper-nav">
-	<nav class="menu-bar">
+		<nav class="menu-bar">
 
-		<ul>
-			<li class="students"><i class="fa fa-graduation-cap fa-2x" aria-hidden="true"></i>
-<a href="gestion-estudiantes.php?select=e">Estudiantes</a></li>
-			<li class="students"><i class="fa fa-university fa-2x" aria-hidden="true"></i>
-<a href="gestion-instituciones.php?select=i">Instituciones</a></li>
-			<li class="students"><i class="fa fa-tasks fa-2x" aria-hidden="true"></i>
-<a href="gestion-programas.php?select=p">Programas</a></li>
-<li class="students"><i class="fa fa-handshake-o fa-2x" aria-hidden="true"></i>
-<a href="gestion-alianzas.php?select=a">Alianzas</a></li>
-		</ul>
-	</nav>
-	</div>
+			<ul>
+				<li class="students"><i class="fa fa-graduation-cap fa-2x" aria-hidden="true"></i>
+					<a href="gestion-estudiantes.php?select=e">Estudiantes</a></li>
+					<li class="students"><i class="fa fa-university fa-2x" aria-hidden="true"></i>
+						<a href="gestion-instituciones.php?select=i">Instituciones</a></li>
+						<li class="students"><i class="fa fa-tasks fa-2x" aria-hidden="true"></i>
+							<a href="gestion-programas.php?select=p">Programas</a></li>
+							<li class="students"><i class="fa fa-handshake-o fa-2x" aria-hidden="true"></i>
+								<a href="gestion-alianzas.php?select=a">Alianzas</a></li>
+							</ul>
+						</nav>
+					</div>
 
-	<div class="contenedor">
+					<div class="contenedor">
--- view/principal-gestion.view.php
+++ view/principal-gestion.view.php
@@ -1,6 +1,28 @@
 <?php require'cabecera-admin.php' ?>
 <div class="background">
-    <img src="../imagenes/gobernacion4.jpg" height="650" width="1349" alt="Gobernación de Risaralda">
+
+    <div class="wraper-odometer"> 
+    	<p>Estudiantes registrados:</p> <br> <p class="odometer" id="odometerE"></p> 
+    </div>
+    <div class="wraper-odometer">
+        <p>Programas registrados:</p> <br> <p class="odometer" id="odometerP"></p>
+    </div>
+    <div class="wraper-odometer"> 
+    	<p>Instituciones registradas:</p> <br> <p class="odometer" id="odometerI"></p> 
+    </div>
+    <div class="wraper-odometer"> 
+    	<p>Alianzas registradas:</p> <br> <p class="odometer" id="odometerA"></p> 
+    </div>
 </div>
+<script>
+setTimeout(function(){
+ odometerE.innerHTML = <?php echo $porceM ?>;
+ odometerP.innerHTML = <?php echo $porceM ?>;
+ odometerI.innerHTML = <?php echo $porceM ?>;
+ odometerA.innerHTML = <?php echo $porceM ?>;
+}, 1000);
+
+</script>
+
 <?php require 'piedepagina-admin.php' ?>
 	
