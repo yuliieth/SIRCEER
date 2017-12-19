b1a62f902b2acd4f438af64c27fc122c0d0335bf
Alexis <titiruizah@gmail.com>
2017-12-18 22:02:49 -0500

    Arreglando pequeños bugs para subir

--- admin/config.php
+++ admin/config.php
@@ -1,19 +1,19 @@
 <?php 
 
 	$bd_config = array(
-		'nameBD' => 'id1872513_sirceer',
-		'host' => 'localhost',
-		'userName' => 'id1872513_root',
-		'pass' => '1088264375C' 
-
-		#'nameBD' => 'sirceer',
+		#'nameBD' => 'id1872513_sirceer',
 		#'host' => 'localhost',
-		#'userName' => 'root',
-		#'pass' => ''
+		#'userName' => 'id1872513_root',
+		#'pass' => '1088264375C' 
+
+		'nameBD' => 'sirceer',
+		'host' => 'localhost',
+		'userName' => 'root',
+		'pass' => ''
 
 		);
 
-	define("URL", "http://sdber.comze.com/");
+	#define("URL", "http://sdber.comze.com/");
 	#define('URL', "http://localhost/SRCEER/");
-	#define('URL', "http://localhost/DesarrolloWeb/PracticaPHP/SRCEER/");
+	define('URL', "http://localhost/DesarrolloWeb/PracticaPHP/SRCEER/");
  ?>
