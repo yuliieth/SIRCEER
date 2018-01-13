bb7ccb49ae3c45a8b24aac6f3d333aafc1253f89
Alexis <titiruizah@gmail.com>
2018-01-12 22:21:30 -0500

    Se elimina la libreria de PHPExcel que se tenia y se descargo una nueva la cual se situo en la carpeta de libs y se avanza mas en el consolidado de datos de los alumnos a importar, en este momento ya se cargan a php

--- gestion/importar-estudiantes.php
+++ gestion/importar-estudiantes.php
@@ -2,7 +2,6 @@
 <?php require_once '../admin/config.php';
 require_once '../php/funciones.php';
 require_once '../php/Conexion.php';
-require '../PHPExcel/IOFactory.php';
 validateSession();
 $cn = getConexion($bd_config);
 comprobarConexion($cn);
@@ -10,19 +9,40 @@ comprobarConexion($cn);
 $estado = false;
 #var_dump($_POST);
 if (isset($_POST['Importar'])) {
-	#var_dump($_FILES);	
+	if ( substr($_FILES['myfile']['name'], -4) == "xlsx" ) {
+
 $nombreArchivo = $_FILES['myfile']['name'];
+
+$destino = "back_".$nombreArchivo;
+
+if (copy($_FILES['myfile']['tmp_name'], $destino)) {
+	echo "Archivo cargado con exito!";
+}else
+{
+	echo "Error al cargar el archivo";
+}
+
+if (file_exists("back_".$nombreArchivo)) {
+
+require_once '../libs/PHPExcel/IOFactory.php';
+
+echo "Exists";
+
 #indicamos que vamos a cargar el archivo
 #llama a la funcion  PHPEXCEL_IOFactory
 #y luego a la funcion load
-$ocjPHPEXCEL = PHPEXCEL_IOFactory::load($nombreArchivo);
+$objPHPEXCEL = PHPEXCEL_IOFactory::load("back_".$nombreArchivo);
 #Establecemos en que hoja vamos a leer por medio del objeto
-$ocjPHPEXCEL->setActiveSheetIndex(0);
+$objPHPEXCEL->setActiveSheetIndex(0);
 #obtenemos el numero de filas en la hoja activa
-$numRows = $ocjPHPEXCEL->setActiveSheetIndex(0)->getHighestRow();
+$numRows = $objPHPEXCEL->setActiveSheetIndex(0)->getHighestRow();
 $estado = true;
-header("Location".URL."gestion/importar-estudiantes.php");
+echo $numRows;
+#Finalmente se hace el insert
+#header("Location:".URL."gestion/importar-estudiantes.php?select=e");
 #https://www.youtube.com/watch?v=4Sw24E5Hi5M
+}
+	}
 }
 
 ?>
--- view/importar-estudiantes.view.php
+++ view/importar-estudiantes.view.php
@@ -1,46 +1,97 @@
 <?php require("cabecera-admin.php") ?>
 <?php require("header-menu.view.php") ?>			
 <!--CONTENIDO-->
-<?php var_dump($_POST); ?>
+<script type="text/javascript">
+	function validarCargue()
+	{
+		if (document.frm_archivo.myfile.value=="") 
+		{
+			alert("Debe cargar una hoja de calculo");
+			document.frm_archivo.myfile.focus();
+			return false;
+		}
+
+		document.frm_archivo.action = "../gestion/importar-estudiantes.php";
+		document.submit();
+	}
+</script>
 <!--Formulario-->
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data" method="post" >
+<form  action="<?php echo $_SERVER['PHP_SELF']; ?>"  name="frm_archivo" enctype="multipart/form-data" method="post" >
 Fichero a recibir: <input type="file" name="myfile"/><br/>
 <input type="submit" value="Enviar" name="Importar">
 </form>
-
-<?php if ($estado): ?>
 	
-<table border=1>
+
+	<?php if ($estado): ?>
+		
+<table>
+<?php 
+	for ($i=2; $i <= $numRows; $i++) { 
+		$tipoDocumento = $objPHPEXCEL ->getActiveSheet()->getCell('A'.$i)->getCalculatedValue();
+		$documento = $objPHPEXCEL ->getActiveSheet()->getCell('B'.$i)->getCalculatedValue();
+		$primerNombre = $objPHPEXCEL ->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
+		$segundoNombre = $objPHPEXCEL ->getActiveSheet()->getCell('D'.$i)->getCalculatedValue();
+		$primerApellido = $objPHPEXCEL ->getActiveSheet()->getCell('E'.$i)->getCalculatedValue();
+		$segundoApellido = $objPHPEXCEL ->getActiveSheet()->getCell('F'.$i)->getCalculatedValue();
+		$tipoSangre = $objPHPEXCEL ->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
+		$telefono = $objPHPEXCEL ->getActiveSheet()->getCell('H'.$i)->getCalculatedValue();
+		$email = $objPHPEXCEL ->getActiveSheet()->getCell('I'.$i)->getCalculatedValue();
+		$fechaNaci = $objPHPEXCEL ->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
+		$edad = $objPHPEXCEL ->getActiveSheet()->getCell('J'.$i)->getCalculatedValue();
+		$muniNaci = $objPHPEXCEL ->getActiveSheet()->getCell('K'.$i)->getCalculatedValue();
+		$direResi = $objPHPEXCEL ->getActiveSheet()->getCell('L'.$i)->getCalculatedValue();
+		$barrioResi = $objPHPEXCEL ->getActiveSheet()->getCell('M'.$i)->getCalculatedValue();
+		$muniResi = $objPHPEXCEL ->getActiveSheet()->getCell('N'.$i)->getCalculatedValue();
+		$estrato = $objPHPEXCEL ->getActiveSheet()->getCell('O'.$i)->getCalculatedValue();
+		$zona = $objPHPEXCEL ->getActiveSheet()->getCell('P'.$i)->getCalculatedValue();
+		$EPS = $objPHPEXCEL ->getActiveSheet()->getCell('Q'.$i)->getCalculatedValue();
+		$situacion = $objPHPEXCEL ->getActiveSheet()->getCell('R'.$i)->getCalculatedValue();
+		$tipoPoblacion = $objPHPEXCEL ->getActiveSheet()->getCell('S'.$i)->getCalculatedValue();
+		$ojos = $objPHPEXCEL ->getActiveSheet()->getCell('T'.$i)->getCalculatedValue();
+		$genero = $objPHPEXCEL ->getActiveSheet()->getCell('U'.$i)->getCalculatedValue();
+		$discapacidad = $objPHPEXCEL ->getActiveSheet()->getCell('U'.$i)->getCalculatedValue();
+		$colegio = $objPHPEXCEL ->getActiveSheet()->getCell('V'.$i)->getCalculatedValue();
+		$situacionAcademica = $objPHPEXCEL ->getActiveSheet()->getCell('W'.$i)->getCalculatedValue();
+		$grado = $objPHPEXCEL ->getActiveSheet()->getCell('X'.$i)->getCalculatedValue();
+		$observacion = $objPHPEXCEL ->getActiveSheet()->getCell('Y'.$i)->getCalculatedValue();
+
+		?>
 	<tr>
-		<td>Nombres</td>
-		<td>Apellidos</td>
-		<td>Telefono</td>
-		<td>Direccion</td>
-		<td>Email</td>
+		<td><?php echo $tipoDocumento ?></td>
+		<td><?php echo $documento ?></td>
+		<td><?php echo $primerNombre ?></td>
+		<td><?php echo $segundoNombre ?></td>
+		<td><?php echo $primerApellido ?></td>
+		<td><?php echo $segundoApellido ?></td>
+		<td><?php echo $tipoSangre ?></td>
+		<td><?php echo $telefono ?></td>
+		<td><?php echo $email ?></td>
+		<td><?php echo $fechaNaci ?></td>
+		<td><?php echo $edad ?></td>
+		<td><?php echo $muniNaci ?></td>
+		<td><?php echo $direResi ?></td>
+		<td><?php echo $barrioResi ?></td>
+		<td><?php echo $muniResi ?></td>
+		<td><?php echo $estrato ?></td>
+		<td><?php echo $zona ?></td>
+		<td><?php echo $EPS ?></td>
+		<td><?php echo $situacion ?></td>
+		<td><?php echo $tipoPoblacion ?></td>
+		<td><?php echo $ojos ?></td>
+		<td><?php echo $genero ?></td>
+		<td><?php echo $discapacidad ?></td>
+		<td><?php echo $colegio ?></td>
+		<td><?php echo $situacionAcademica ?></td>
+		<td><?php echo $grado ?></td>
+		<td><?php echo $observacion ?></td>
 	</tr>
 
-<?php 
-	for ($i=1; $i <= $numRows ; $i++) { 
-		$nombres = $ocjPHPEXCEL->getActiveSheet()->getCell('A'.$i)->getCalculatedValue();
-		$apellidos = $ocjPHPEXCEL->getActiveSheet()->getCell('B'.$i)->getCalculatedValue();
-		$telefeno = $ocjPHPEXCEL->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
-		$direccion = $ocjPHPEXCEL->getActiveSheet()->getCell('D'.$i)->getCalculatedValue();
-		$email = $ocjPHPEXCEL->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
-
-
-	}?>
-	<tr>
- 		<td><?php echo $nombres ?></td>
- 		<td><?php echo $apellidos ?></td>
- 		<td><?php echo $telefeno ?></td>
- 		<td><?php echo $direccion ?></td>
- 		<td><?php echo $email ?></td>
-	
-	</tr> 
+		
+		<?php 
+	}
+ ?>
 </table>
-<?php endif ?>
-	
-
+	<?php endif ?>
 <!--END CONTENIDO-->
 <?php require("footer-menu.view.php") ?>					
 <?php require("piedepagina-admin.php") ?>
